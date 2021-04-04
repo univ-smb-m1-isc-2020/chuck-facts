7699ea7be0be33b64681c8a94e80bfe065933889
gjambet <guillaume.jambet@gmail.com>
2021-04-05 00:41:00 +0200

    chore : cleaned initialization

--- src/main/java/fr/univ_smb/isc/m1/chuck_facts/application/ChuckFactsService.java
+++ src/main/java/fr/univ_smb/isc/m1/chuck_facts/application/ChuckFactsService.java
@@ -4,7 +4,6 @@ import fr.univ_smb.isc.m1.chuck_facts.infrastructure.persistence.ChuckFact;
 import fr.univ_smb.isc.m1.chuck_facts.infrastructure.persistence.ChuckFactsRepository;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service
@@ -14,14 +13,6 @@ public class ChuckFactsService {
 
     public ChuckFactsService(ChuckFactsRepository repository) {
         this.repository = repository;
-    }
-
-    @PostConstruct
-    public void initialize() {
-        if (repository.findAll().isEmpty()) {
-            repository.saveAndFlush(new ChuckFact("Chuck Norris can divide by zero."));
-            repository.saveAndFlush(new ChuckFact("Chuck Norris once lost his wedding ring....since then it's been war in Middle Earth"));
-        }
     }
 
     public List<ChuckFact> facts() {
